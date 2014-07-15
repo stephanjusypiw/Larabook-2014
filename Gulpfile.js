9e11eb23d68c049bc0ed331777208970abacc302
Stephan Jusypiw <stephanj@internode.on.net>
2014-07-15 17:29:36 +1000

    Initial commit after lesson 6

---
+++ Gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('css', function(){
+    gulp.src('app/assets/sass/main.scss')
+        .pipe(sass())
+        .pipe(autoprefixer('Last 10 version'))
+        .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('watch', function(){
+    gulp.watch('app/assets/sass/**/*.scss', ['css']);
+});
+
+gulp.task('default',['watch']);
+
