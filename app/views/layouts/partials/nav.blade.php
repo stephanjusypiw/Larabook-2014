18725ff0a8990feee6a33f3663c6416ac352f878
Stephan Jusypiw <stephanj@internode.on.net>
2014-07-27 15:29:25 +1000

    Completed after Part 20 - Dummy Users and Statuses

--- app/views/layouts/partials/nav.blade.php
+++ app/views/layouts/partials/nav.blade.php
@@ -7,7 +7,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Larabook</a>
+            <a class="navbar-brand" href="">Larabook</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
--- app/views/statuses/index.blade.php
+++ app/views/statuses/index.blade.php
@@ -9,7 +9,7 @@
         <div class="status-post">
             {{ Form::open() }}
             <div class="form-group">
-                {{ Form::textarea('body', null, ['class' => 'form-control', 'rows' => 3, 'placeholder' => "What's on your mind"]) }}
+                {{ Form::textarea('body', null, ['class' => 'form-control', 'rows' => 3, 'placeholder' => "What's on your mind?"]) }}
             </div>
             <div class="form-group status-post-submit">
                 {{ Form::submit('Post Status', ['class' => 'btn btn-default btn-xs']) }}
